1d6f36636e57a9abd97522669e43f5bf12013274
Tanisha verma <tanishavermaa4@gmail.com>
2025-11-30 00:21:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Mission from './components/Mission';
 import Contact from './components/Contact';
-// import Footer from './components/Footer';
 
 function App() {
   return (
